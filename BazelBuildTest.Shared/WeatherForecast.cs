c0d2a958cab97912743eb8c9be8259d735913f34
Michael Shobowale <michaelshoboske@gmail.com>
2026-04-18 14:42:30 +0300

    chore: change to see CI

--- BazelBuildTest.Shared/WeatherForecast.cs
+++ BazelBuildTest.Shared/WeatherForecast.cs
@@ -4,5 +4,5 @@ namespace BazelBuildTest.Shared;
 
 public record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 58 + (int)(TemperatureC / 0.5556);
 }

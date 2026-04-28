3c3e81a689275fed3fb69dac0348d8397cbfcb97
Michael Shobowale <58557722+shoboske@users.noreply.github.com>
2026-04-28 10:49:52 +0300

    chore: Remove 'Freezing' from weather summaries to test delta difference

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
